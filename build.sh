c41088bcb8e24ccff54834410ad7841ca45d57b6
capoe <cp605@cam.ac.uk>
2020-05-07 12:55:16 +0100

    Python config in build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ fi
 echo "Installing gylm ..."
 mkdir -p build
 cd build
-cmake .. -DPYBIND11_PYTHON_VERSION=3.6 && make -j 4 && make install
+cmake .. \
+    -DPYBIND11_PYTHON_VERSION=3.6 \
+    -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
+    -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
+    && make -j 4 && make install
 cd ..
 
