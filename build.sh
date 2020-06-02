091a59f70d91b766694a4a18db667dffbf30f9c8
capoe <carlpoelking@gmail.com>
2020-06-02 19:09:33 +0100

    Python version.

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 if [ ! -d "pybind11" ]; then
     git clone https://github.com/pybind/pybind11.git
 fi
+py_version=$(python -c "import sys; print('.'.join(sys.version.split()[0].split('.')[0:2]))")
 
-echo "Installing gylm ..."
+echo "Installing gylm using python=${py_version}"
 mkdir -p build
 cd build
 cmake .. \
-    -DPYBIND11_PYTHON_VERSION=3.6 \
+    -DPYBIND11_PYTHON_VERSION=${py_version} \
     -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
     -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
     && make -j 4 && make install
